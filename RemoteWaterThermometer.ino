a18711e526bda0f6628bd70da97794e4266c1fc9
Petr Stehlík <petr@pstehlik.cz>
2013-07-24 16:26:51 +0200

    power saving features: - sleep mode - powering of transmitter only when needed (doesn't seem to help save power) - powering of one wire bus only when needed (doesn't work, temperature 0.0, so disabled) - lesser precision (11 bit is enough) to save time thus power

--- RemoteWaterThermometer.ino
+++ RemoteWaterThermometer.ino
@@ -6,19 +6,24 @@
  * GPL
  */
 
+#include <avr/wdt.h>
+#include <avr/sleep.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
 #include "WaterTempTransmitter.h"
 
-WaterTempTransmitter tx=WaterTempTransmitter(3 /* transmit Arduino pin */, 0x00 /* sensor ID */, 2 /* transmit channel */);
+#define ONE_WIRE_BUS_PIN    2    // Data wire is plugged into port 2 on the Arduino
+#define TX433MHZ_PIN        3    // Transmitter Arduino pin
+#define BUS_POWER_PIN       4    // power for DS18B20 one wire bus - doesn't work well for parasite mode
+#define TX_POWER_PIN        5    // power for transmitter
 
-// Data wire is plugged into port 2 on the Arduino
-#define ONE_WIRE_BUS 2
-#define TEMPERATURE_PRECISION 12
+WaterTempTransmitter tx=WaterTempTransmitter(TX433MHZ_PIN, 0x00 /* sensor ID */, 2 /* transmit channel */);
+
+#define TEMPERATURE_PRECISION 11
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
+OneWire oneWire(ONE_WIRE_BUS_PIN);
 
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
@@ -31,8 +36,10 @@ void setup()
     Serial.begin(9600);
     Serial.println("DS18B20 433 MHz Thermometer");
     
-    pinMode(13, OUTPUT);
-    digitalWrite(13, LOW);
+    pinMode(BUS_POWER_PIN, OUTPUT);
+    pinMode(TX_POWER_PIN, OUTPUT);
+    
+    digitalWrite(BUS_POWER_PIN, HIGH);    // power the one wire sensors bus
 
     // Start up the library
     sensors.begin();
@@ -57,11 +64,7 @@ void setup()
     else Serial.println("OFF");
 
     if(sensors.getAddress(tempDeviceAddress, 0)) {
-        Serial.print("Found device with address: ");
-        printAddress(tempDeviceAddress);
-        Serial.println();
-
-        Serial.print("Setting resolution to ");
+        Serial.print("Device found, setting resolution to ");
         Serial.println(TEMPERATURE_PRECISION, DEC);
 
         // set the resolution to TEMPERATURE_PRECISION bit (Each Dallas/Maxim device is capable of several different resolutions)
@@ -73,6 +76,7 @@ void setup()
     }
     else {
         Serial.print("Found ghost device but could not detect address. Check power and cabling");
+        while(true) ;
     }
 }
 
@@ -80,22 +84,31 @@ void loop()
 {
     Serial.print("Requesting temperatures...");
     sensors.requestTemperatures(); // Send the command to get temperatures
-    if(sensors.getAddress(tempDeviceAddress, 0))
-    {
-        // It responds almost immediately. Let's print out the data
+    boolean ret = sensors.getAddress(tempDeviceAddress, 0);
+    if (ret) {
+        // transmit the temperature
+        digitalWrite(TX_POWER_PIN, HIGH);    // power the transmitter
+        delay(10);
         transmitTemperature(tempDeviceAddress); // Use a simple function to print out the data
-        delay(3000);
+        delay(10);
+        digitalWrite(TX_POWER_PIN, LOW);     // disable the transmitter
+    }
+
+    for(int i=0; i<3; i++) {
+        wdt_reset();                         // Get ready to go to sleep...
+        watchdogEnable();                    // Turn on the watchdog timer
+        sleepNow();                          // Go to sleep, watchdog timer will wake later
     }
 }
-
-// function to print a device address
-void printAddress(DeviceAddress deviceAddress)
-{
-    for (uint8_t i = 0; i < 8; i++)
-    {
-        if (deviceAddress[i] < 16) Serial.print("0");
-        Serial.print(deviceAddress[i], HEX);
-    }
+ 
+void watchdogEnable() {                // Turn on watchdog timer; interrupt mode every 8.0s
+    cli();
+    MCUSR = 0;
+    WDTCSR |= B00011000;
+    //WDTCSR = B01000111;                // 2 Second Timeout
+    //WDTCSR = B01100000;                // 4 Second Timeout
+    WDTCSR = B01100001;                  // 8 Second Timeout
+    sei();
 }
 
 // function to transmit the temperature of a device
@@ -103,11 +116,44 @@ void transmitTemperature(DeviceAddress deviceAddress)
 {
     float tempC = sensors.getTempC(deviceAddress);
 
-    digitalWrite(13, HIGH);
     tx.send(tempC);
-    digitalWrite(13, LOW);
 
     Serial.print(" ");
     Serial.println(tempC);
 }
 
+void sleepNow()
+{
+  /* Now is the time to set the sleep mode. In the Atmega8 datasheet
+   * http://www.atmel.com/dyn/resources/prod_documents/doc2486.pdf on page 35
+   * there is a list of sleep modes which explains which clocks and 
+   * wake up sources are available in which sleep modes.
+   *
+   * In the avr/sleep.h file, the call names of these sleep modus are to be found:
+   *
+   * The 5 different modes are:
+   *     SLEEP_MODE_IDLE         -the least power savings 
+   *     SLEEP_MODE_ADC
+   *     SLEEP_MODE_PWR_SAVE
+   *     SLEEP_MODE_STANDBY
+   *     SLEEP_MODE_PWR_DOWN     -the most power savings
+   *
+   *  the power reduction management &lt;avr/power.h&gt;  is described in 
+   *  http://www.nongnu.org/avr-libc/user-manual/group__avr__power.html
+   */
+      
+  set_sleep_mode(SLEEP_MODE_PWR_SAVE); // Sleep mode is set here
+   
+  sleep_enable();                      // Enables the sleep bit in the mcucr register
+                                       // so sleep is possible. just a safety pin 
+  sleep_mode();                        // Here the device is actually put to sleep!!
+                                       // THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP
+  sleep_disable();                     // Dirst thing after waking from sleep:
+                                       // disable sleep...
+}
+
+ISR (WDT_vect) {                       // WDT Wakeup
+    cli();
+    wdt_disable();
+    sei();
+}
